ec095e1d3714774b5846a5255f5e4d480002c8d6
Psycho <93525954+itspsycholol322@users.noreply.github.com>
2021-12-17 17:22:22 +0200

    intial commit - project complete

---
+++ main.js
@@ -0,0 +1,48 @@
+let newYearsDate = "1 Jan 2022";
+let daysLeft = document.querySelector(".days");
+let hoursLeft = document.querySelector(".hours");
+let minutesLeft = document.querySelector(".minutes");
+let secondsLeft = document.querySelector(".seconds");
+
+let result = document.querySelector(".results");
+let header = document.querySelector(".header");
+let container = document.querySelector(".container");
+
+setInterval(getDate, 1000);
+getDate();
+setInterval(addSnowFlakes, 100);
+
+function getDate() {
+  let wantedDate = new Date(newYearsDate);
+  let currentDate = new Date();
+
+  let totalSeconds = (wantedDate - currentDate) / 1000;
+
+  days = Math.floor(totalSeconds / 3600 / 24);
+  hours = Math.floor(totalSeconds / 3600) % 24;
+  minutes = Math.floor(totalSeconds / 60) % 60;
+  seconds = Math.floor(totalSeconds) % 60;
+
+  daysLeft.innerHTML = days;
+  hoursLeft.innerHTML = hours;
+  minutesLeft.innerHTML = minutes;
+  secondsLeft.innerHTML = seconds;
+
+  if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+    result.innerHTML = `<h1> ITS 2022 <br> HAPPY NEW YEAR </h1>`;
+    header.style.display = "none";
+  }
+}
+
+function addSnowFlakes() {
+  let snowflake = document.createElement("i");
+  snowflake.classList.add("fas");
+  snowflake.classList.add("fa-snowflake");
+  snowflake.style.left = `${Math.random() * container.clientWidth}px`;
+  snowflake.style.opacity = Math.random();
+  snowflake.style.animationDuration = `${Math.random() * 5 + 5}s`;
+  setTimeout(() => {
+    snowflake.remove();
+  }, Math.random() * 5000 + 5000);
+  container.appendChild(snowflake);
+}
